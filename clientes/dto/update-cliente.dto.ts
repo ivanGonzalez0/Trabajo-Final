fd852153c4f716707c6f1bbbef71e5b4ce07c158
Juan <juanccamposrey@gmail.com>
2025-09-22 20:11:28 -0300

    restructuracion de carpetas

--- clientes/dto/update-cliente.dto.ts
+++ clientes/dto/update-cliente.dto.ts
@@ -1,15 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateClienteDto } from './create-cliente.dto';
-import { IsOptional,IsString,IsEmail, IsNotEmpty } from 'class-validator';
+import { IsOptional, IsString, IsEmail, IsNotEmpty } from 'class-validator';
 
 export class UpdateClienteDto {
     @IsNotEmpty()
     @IsOptional()
     @IsString()
-        contraseña:string;
-        @IsNotEmpty()
+    contraseña: string;
+
+    @IsNotEmpty()
     @IsOptional()
-        @IsEmail()
-        email:string;
+    @IsEmail()
+    email: string;
 
 }
